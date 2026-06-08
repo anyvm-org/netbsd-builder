f03c26d597a783545b721983f200a82d50f0f151
neil <gitpc@neilpang.com>
2026-06-08 20:04:58 +0200

    add 11.0 rc4

--- hooks/vm_postBuild.sh
+++ hooks/vm_postBuild.sh
@@ -1,7 +1,9 @@
 
 
-echo 'ip6mode="none"' >> /etc/rc.conf
-
+# NetBSD's ip6mode only accepts autohost/host/router; ip6mode="none" was never
+# valid -- a silent no-op before 11.0, a boot-time `WARNING: invalid $ip6mode
+# value "none"` on 11.0+ -- so it is dropped. IPv4-only is enforced by
+# dhcpcd_flags="-4" below.
 echo 'dhcpcd_flags="-4"' >> /etc/rc.conf
 
 
